2f5044146408ca074f3cfdd5b8ffdc8b8a3d54ed
AandrzejMi <andrzej.mi@op.pl>
2019-10-24 17:03:09 +0200

    NavMesh demo

---
+++ Assets/MouseInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseInput : MonoBehaviour
+{
+    public UnityEvent OnLeftButton, OnRigthButton;
+
+    private void Update()
+    {
+        if( Input.GetMouseButtonDown(0) )
+        {
+            OnLeftButton.Invoke();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            OnRigthButton.Invoke();
+        }
+    }
+
+}
---
+++ Assets/TmpController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TmpController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private NavMeshAgent agent;
+
+    public bool playerControlled = false;
+
+    public GameObject target;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if ( target != null && !playerControlled )
+        {
+            agent.SetDestination(target.transform.position);
+        }
+    }
+
+    public void GoToMousePosition()
+    {
+        agent.destination = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+}
