a558f5fe4888ffe3abe0da983077ee3510a3afb5
kunkap <pawelkun@gmail.com>
2019-11-02 17:10:10 +0100

    Abstract Enemy class and PatrollingEnemy class in progress

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+#pragma warning disable
+    [SerializeField]
+    private float eyesightRange;
+    [SerializeField]
+    private float shootingRange;
+    [SerializeField]
+    private float chaseRange;
+#pragma warning restore
+
+    private Transform chasedTransform;
+}
---
+++ Assets/Scripts/PatrollingEnemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public class PatrollingEnemy : Enemy
+{
+#pragma warning disable
+    [SerializeField]
+    private Vector2[] points;
+#pragma warning restore
+
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        agent.autoBraking = false;
+        GotoNextPoint();
+    }
+
+    private void Update()
+    {
+        if(!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+
+    }
+
+    private void GotoNextPoint()
+    {
+        if(points.Length == 0)
+        {
+            return;
+        }
+
+        agent.destination = (Vector3)points[destPoint];
+
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+}
