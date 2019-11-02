d0a5c5d6db8a43294bd5afb5ad83b7b73c812245
AandrzejMi <andrzej.mi@op.pl>
2019-11-02 13:25:03 +0100

    shooting

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if( instance == null )
+            {
+                GameObject obj = new GameObject("GameManager");
+                return instance = obj.AddComponent<GameManager>();
+            }
+            return instance;
+        }
+    }
+
+    private void OnValidate()
+    {
+        if( instance == null )
+        {
+            instance = this;
+        }
+        else if( instance != this )
+        {
+            Debug.LogError("Duplicated GameManager");
+        }
+    }
+
+    public List<GameObject> ourCrew = new List<GameObject>();
+    public List<GameObject> enemies = new List<GameObject>();
+
+
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public bool enemy = true;
+    public float baseDamage = 5;
+    public float attackInterval = 1.5f;
+
+    public GameObject bulletPrefab;
+    public Vector2 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if( enemy )
+        {
+            GameManager.Instance.enemies.Add(gameObject);
+        }
+        else
+        {
+            GameManager.Instance.ourCrew.Add(gameObject);
+        }
+
+        IEnumerator aaa()
+        {
+            while (true)
+            {
+                Debug.Log("Aaa");
+                ShootTo(target);
+                yield return new WaitForSeconds(attackInterval);
+            }
+        }
+
+        StartCoroutine(aaa());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShootTo( Vector2 target )
+    {
+        Instantiate(bulletPrefab,transform).GetComponent<Bullet>().Shoot( target - (Vector2)transform.position);
+    }
+
+    public GameObject Nearstemy
+    {
+        get
+        {
+            GameObject target = null;
+            float distance = float.PositiveInfinity;
+            foreach (GameObject enemy in Enemies)
+            {
+                if (Vector2.Distance(transform.position, enemy.transform.position) < distance)
+                {
+                    target = enemy;
+                }
+            }
+            return target;
+        }
+    }
+
+    public List<GameObject> Enemies
+    {
+        get
+        {
+            if (!enemy)
+                return GameManager.Instance.enemies;
+            else
+                return GameManager.Instance.ourCrew;
+        }
+    }
+}
