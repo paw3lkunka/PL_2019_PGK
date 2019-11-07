8f5a8362543952f618f47761df2cf60de639642f
kunkap <pawelkun@gmail.com>
2019-11-07 07:08:36 +0100

    Tutorial almost ready, it lacks some ready-to-use components to make it actual part of the game, but Andrew is on it

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -12,17 +12,23 @@ public class TutorialController : MonoBehaviour
     private GameObject smokeWater;
     [SerializeField]
     [Range(0.0f, 1.0f)]
+    private float startWaterLevel;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
     private float minimumWaterLevel;
     [SerializeField]
     private GameObject smokeFaith;
+    [Range(0.0f, 1.0f)]
+    [SerializeField]
+    private float startFaithLevel;
     [Range(0.0f, 1.0f)]
     [SerializeField]
     private float minimumFaithLevel;
 
     private void Start()
     {
-        GameManager.Instance.Water = 0.05f;
-        GameManager.Instance.Faith = 0.5f;
+        GameManager.Instance.Water = startWaterLevel;
+        GameManager.Instance.Faith = startFaithLevel;
     }
 
     private void Update()
