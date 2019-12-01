85d19b06f41043e184cbfeb83ada246220460a68
Ziemniak <potatoosalad@gmail.com>
2019-12-01 21:50:16 +0100

    Better GUI, Cheat Sheet, Information Bar class - Work in progress

---
+++ Assets/Scripts/UI Scripts/GUI.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class GUI : MonoBehaviour
+{
+#pragma warning disable
+    [SerializeField] private Slider faithBar;
+    [SerializeField] private Slider waterBar;
+    [SerializeField] private TextMeshProUGUI faithText;
+    [SerializeField] private TextMeshProUGUI waterText;
+    [SerializeField] private TextMeshProUGUI cultistsText;
+#pragma warning restore
+
+    public float FaithIndicator
+    {
+        set
+        {
+            faithBar.value = value;
+            faithText.text = "Faith: " + (int)(value * 100) + "%";
+        }
+        get => faithBar.value;
+    }
+    public float WaterIndicator
+    {
+        set
+        {
+            waterBar.value = value;
+            waterText.text = "Water: " + (int)(value * 100) + "%";
+        }
+        get => waterBar.value;
+    }
+
+    public int CrewIndicator
+    {
+        set
+        {
+            cultistsText.text = "Cultists: " + value;
+        }
+    }
+
+    public Canvas UICanvas
+    {
+        get => GetComponent<Canvas>();
+    }
+
+    private Image faithImage;
+    private Image waterImage;
+
+    private readonly Color32 faithMedium = new Color32(255, 133, 0, 255); // orange
+    private readonly Color32 faithOverflow = new Color32(255, 0, 0, 255); // red
+    private readonly Color32 faithLow = new Color32(255, 200, 138, 255); // light orange
+
+    private readonly Color32 waterGood = new Color32(0, 40, 255, 255); // blue
+    private readonly Color32 waterBad = new Color32(108, 130, 255, 255); // light blue
+    
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        faithImage = faithBar.transform.GetChild(1).transform.GetChild(0).GetComponent<Image>();
+        waterImage = waterBar.transform.GetChild(1).transform.GetChild(0).GetComponent<Image>();
+    }
+
+    public void Update()
+    {
+        CrewIndicator = GameManager.Instance.cultistNumber;
+        WaterIndicator = GameManager.Instance.Water;
+        FaithIndicator = GameManager.Instance.Faith;
+
+        if(FaithIndicator >= 0.9f)
+        {
+            faithImage.color = faithOverflow;
+        }
+        else if(FaithIndicator <= 0.2f)
+        {
+            faithImage.color = faithLow;
+        }
+        else
+        {
+            faithImage.color = faithMedium;
+        }
+
+        if(WaterIndicator <= 0.2f)
+        {
+            waterImage.color = waterBad;
+        }
+        else
+        {
+            waterImage.color = waterGood;
+        }
+    }
+}
---
+++ Assets/Scripts/UI Scripts/InformationBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InformationBar
+{
+    private GameObject prefab;
+
+    public int MaxLifeTime { get; private set; }
+
+    public int LifeTime;
+
+    InformationBar(GameObject uiPrefab, string information)
+    {
+        prefab = uiPrefab;
+        prefab.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = information;
+        MaxLifeTime = 8;
+    }
+}
