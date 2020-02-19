f9b21b31405f76c26e957237f731df104fbfa52b
AandrzejMi <andrzej.mi@op.pl>
2020-02-19 19:28:39 +0100

    CombatArea united with Location

--- Assets/Scripts/MapSystem/Location.cs
+++ Assets/Scripts/MapSystem/Location.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-[System.Serializable]
+[RequireComponent(typeof(Collider2D))]
 public class Location : MonoBehaviour
 {
     #region Variables
 
     public int generationID;
+
+    public string sceneName;
+    public Vector2 returnPoint;
+
+    public Vector2 GlobalReturnPoint => (Vector2)transform.position + returnPoint;
 
     #endregion
 
@@ -16,6 +22,19 @@ public class Location : MonoBehaviour
     private void OnValidate()
     {
         transform.position = new Vector2(Mathf.Round(transform.position.x), Mathf.Round(transform.position.y));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameManager.Instance.savedPosition = GlobalReturnPoint;
+        SceneManager.LoadScene(sceneName);
+        GameManager.Instance.OnLocationEnterInvoke();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(GlobalReturnPoint, .2f);
     }
 
     #endregion
