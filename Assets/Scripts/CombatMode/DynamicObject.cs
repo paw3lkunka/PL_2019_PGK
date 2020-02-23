e80a3a5797c7a431671805c6ddb49af356571a25
AandrzejMi <andrzej.mi@op.pl>
2020-02-23 23:36:24 +0100

    FFixes from meating + script order

--- Assets/Scripts/CombatMode/DynamicObject.cs
+++ Assets/Scripts/CombatMode/DynamicObject.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -27,17 +27,10 @@ public class DynamicObject : MonoBehaviour
         {
             value = GameManager.Instance.destroyedDynamicObjects[GameManager.Instance.currentLocation];
         }
-        catch(Exception exc)
+        catch(KeyNotFoundException)
         {
-            if(exc is KeyNotFoundException || exc is ArgumentNullException)
-            {
-                GameManager.Instance.destroyedDynamicObjects.Add(GameManager.Instance.currentLocation, new HashSet<int>());
-                value = GameManager.Instance.destroyedDynamicObjects[GameManager.Instance.currentLocation];
-            }
-            else
-            {
-                throw exc;
-            }
+            GameManager.Instance.destroyedDynamicObjects.Add(GameManager.Instance.currentLocation, new HashSet<int>());
+            value = GameManager.Instance.destroyedDynamicObjects[GameManager.Instance.currentLocation];
         }
         finally
         {
