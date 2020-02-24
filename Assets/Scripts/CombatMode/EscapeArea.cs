bf1b766c2c9c2add1ae3318a412660d8658e7e07
PawelKunka <pawelkun@gmail.com>
2020-02-24 19:10:43 +0100

    Fixed leaving area by touching EscapeArea with cultist, it must now be a CultLeader

--- Assets/Scripts/CombatMode/EscapeArea.cs
+++ Assets/Scripts/CombatMode/EscapeArea.cs
@@ -16,7 +16,7 @@ public class EscapeArea : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (GameManager.Instance.ourCrew.IndexOf(collision.gameObject) != -1)
+        if (GameManager.Instance.ourCrew.IndexOf(collision.gameObject) == 0)
         {
             GameManager.Instance.OnLocationExitInvoke();
             SceneManager.LoadScene("MainMap");
