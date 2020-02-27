ed6b7802848c34589b19d0473ec79bff3773febd
Filip Mazurek <f.maz.98@outlook.com>
2020-02-27 15:11:58 +0100

    Updated tutorial scene design

---
+++ Assets/Scripts/3D/CopyTransform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyTransform : MonoBehaviour
+{
+#pragma warning disable
+    [SerializeField] private Transform objectToFollow;
+    [SerializeField] private string objectName;
+    [SerializeField] private bool position;
+    [SerializeField] private bool rotation;
+    [SerializeField] private bool scale;
+#pragma warning restore
+
+    private void Start() 
+    {
+        if (objectToFollow == null)
+        {
+            objectToFollow = GameObject.Find(objectName).transform;
+        }
+    }
+
+    void Update()
+    {
+        if (position)
+            transform.position = objectToFollow.transform.position;
+
+        if (rotation)
+            transform.rotation = objectToFollow.transform.rotation;
+
+        if (scale)
+            transform.localScale = objectToFollow.transform.localScale;
+    }
+}
