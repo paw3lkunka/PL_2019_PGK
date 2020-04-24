37f9747b624fb22b064aa66b13ff7d2befe8b98b
Filip Mazurek <f.maz.98@outlook.com>
2020-04-24 17:09:17 +0200

    Reimplemented main menu based on stack ui

---
+++ Assets/Scripts/UI Scripts/MainMenu/DifficultyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyController : MonoBehaviour
+{
+    public bool SkipTutorial { get; set; }
+    public bool EnableCheats { get; set; }
+    public bool DebugScene { get; set; }
+
+    public void Easy()
+    {
+        ApplicationManager.Instance.enableCheats = EnableCheats;
+        ApplicationManager.Instance.skipTutorial = SkipTutorial;
+        ApplicationManager.Instance.StartGame(DebugScene ? GameMode.Debug : GameMode.Normal, Difficulty.Easy);
+    }
+
+    public void Hard()
+    {
+        ApplicationManager.Instance.enableCheats = EnableCheats;
+        ApplicationManager.Instance.skipTutorial = SkipTutorial;
+        ApplicationManager.Instance.StartGame(DebugScene ? GameMode.Debug : GameMode.Normal, Difficulty.Hard);
+    }
+}
