058ed116a6860a81e4f9d06195f0b5b45aaf6799
Filip Mazurek <f.maz.98@outlook.com>
2020-05-06 23:27:32 +0200

    Added simple world map skeleton and resource depletion mechanism

---
+++ Assets/Scripts/AgentSystem/BehaviourWorldMapInput.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BehaviourWorldMapInput : MonoBehaviour
+{
+    private Moveable moveable;
+    //todo position in formation
+    public Vector3 formationOffset = Vector3.zero;
+
+    private void GoToCursorPosition(InputAction.CallbackContext ctx)
+    {
+        var inputValue = Mouse.current.position.ReadValue();
+        var ray = Camera.main.ScreenPointToRay(inputValue);
+
+        foreach (var hit in Physics.RaycastAll(ray, 100))
+        {
+            // TODO: replace tag with layer mask
+            if (hit.collider.CompareTag("Ground"))
+            {
+                moveable.Go(hit.point);
+                continue;
+            }
+        }
+    }
+
+
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        moveable = GetComponent<Moveable>();
+    }
+
+    private void OnEnable()
+    {
+        ApplicationManager.Instance.Input.Gameplay.SetWalkTarget.performed += GoToCursorPosition;
+        ApplicationManager.Instance.Input.Gameplay.SetWalkTarget.Enable();
+    }
+
+    private void OnDisable()
+    {
+        ApplicationManager.Instance.Input.Gameplay.SetWalkTarget.performed -= GoToCursorPosition;
+        ApplicationManager.Instance.Input.Gameplay.SetWalkTarget.Disable();
+    }
+
+    #endregion
+}
