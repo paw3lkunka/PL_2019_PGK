38728bfe043f522977d1b918648c61e6df3c6e65
AandrzejMi <andrzej.mi@op.pl>
2020-07-01 13:12:24 +0200

    Wall script more universal

--- Assets/Scripts/WorldMap/RockWallChunk.cs
+++ Assets/Scripts/WorldMap/RockWallChunk.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class RockWallChunk : MonoBehaviour
 {
+    protected bool isOriginal = true;
+    public int instances = 3;
     public int index = 0;
     public float width = 100;
     public Vector3 zeroPos = Vector3.zero;
     public Vector3 axis = Vector3.right;
+    public bool enableTeleportation = true;
 
     private void OnValidate()
     {
         axis.Normalize();
     }
 
+    private void Start()
+    {
+        //Will blow up - if will be in awake
+        if (isOriginal)
+        {
+            GeneratreInstances();
+        }
+        Teleport();
+    }
+
     private void Update()
+    {
+        if (enableTeleportation)
+        {
+            Teleport();
+        }
+    }
+
+    public void GeneratreInstances()
+    {        
+        for (int i = 0; i < instances; i++ )
+        {
+            var clone = Instantiate(gameObject, transform.parent).GetComponent<RockWallChunk>();
+            clone.isOriginal = false;
+            clone.index = i % 2 != 0 ? i / 2 : -i / 2;
+        }
+    }
+
+    private void Teleport()
     {
         Vector3 cultPos = WorldSceneManager.Instance.cult.transform.position;
         Vector3 project = Vector3.Project(cultPos, axis);
