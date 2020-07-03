93edda9fcdf3e72ef1a1e1673956e8f5abc03ed1
Filip Mazurek <f.maz.98@outlook.com>
2020-07-03 13:03:37 +0200

    Added world map obstacles and canion location in world map

---
+++ Assets/Scripts/WorldMap/Billboard.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public bool X = false;
+    public bool Y = true;
+    public bool Z = false;
+
+    private Camera mainCam;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 camRotation = mainCam.transform.rotation.eulerAngles;
+        Vector3 thisRotation = transform.rotation.eulerAngles;
+        if (!X)
+        {
+            camRotation.x = thisRotation.x;
+        }
+        if (!Y)
+        {
+            camRotation.y = thisRotation.y;
+        }
+        if (!Z)
+        {
+            camRotation.z = thisRotation.z;
+        }
+
+        transform.rotation = Quaternion.Euler(camRotation);
+    }
+}
--- Assets/Scripts/WorldMap/EnviroObstacle.cs
+++ Assets/Scripts/WorldMap/EnviroObstacle.cs
@@ -3,24 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(NavMeshObstacle))]
 public class EnviroObstacle : EnviroObject
 {
     [ContextMenu("Randomize")]
     public override void Randomize()
     {
         transform.rotation = Quaternion.AngleAxis(Random.Range(0,360), Vector3.up);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
