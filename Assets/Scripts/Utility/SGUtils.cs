8988ab6b58956c8f45025237daf51437208478e5
AandrzejMi <andrzej.mi@op.pl>
2020-07-08 19:06:41 +0200

    Free centre option redesigned

---
+++ Assets/Scripts/Utility/SGUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SGUtils
+{
+    public static void SafeDestroy(Object obj)
+    {
+#      if UNITY_EDITOR
+            if (Application.isEditor)
+            {
+                Object.DestroyImmediate(obj);
+            }
+            else
+#      endif
+            {
+                Object.Destroy(obj);
+            }
+    }
+}
