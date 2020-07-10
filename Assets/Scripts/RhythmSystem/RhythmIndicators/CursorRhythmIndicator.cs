6e4fcd387403336fae2e40cb42e8ac1d29099899
Filip Mazurek <f.maz.98@outlook.com>
2020-07-10 18:07:20 +0200

    Added new 3D cursor

--- Assets/Scripts/RhythmSystem/RhythmIndicators/CursorRhythmIndicator.cs
+++ Assets/Scripts/RhythmSystem/RhythmIndicators/CursorRhythmIndicator.cs
@@ -8,17 +8,17 @@ public class CursorRhythmIndicator : MonoBehaviour
 
 #pragma warning disable
     [Header("Rhythm pulse indicator")]
-    [SerializeField] private SpriteRenderer rhythmIndicator;
+    [SerializeField] private MeshRenderer rhythmIndicator;
     [SerializeField] private AnimationCurve rhythmCurve;
     [SerializeField] private float rhytmIndicatorMaxSize = 5.0f;
 
     [Space]
     
     [Header("Hit indicator")]
-    [SerializeField] private SpriteRenderer hitIndicator;
+    [SerializeField] private MeshRenderer hitIndicator;
     [SerializeField] private AnimationCurve hitCurve;
     [SerializeField] private float hitIndicatorMaxSize = 3.0f;
-    [SerializeField] private List<SpriteRenderer> beatHitSigns = new List<SpriteRenderer>(3);
+    [SerializeField] private List<MeshRenderer> beatHitSigns = new List<MeshRenderer>(4);
     
     [Space]
 
@@ -36,7 +36,7 @@ public class CursorRhythmIndicator : MonoBehaviour
 
     private void Awake()
     {
-        hitIndicator.color = noneHitColor;
+        hitIndicator.sharedMaterial.color = noneHitColor;
     }
 
     private void OnEnable()
@@ -62,16 +62,16 @@ public class CursorRhythmIndicator : MonoBehaviour
             var indicatorFactor = AudioTimeline.Instance.NormalizedTimeUntilNextBeat;
 
             var rhythmSize = rhytmIndicatorMaxSize - ((rhytmIndicatorMaxSize - 1) * (1.0f - indicatorFactor));
-            rhythmIndicator.transform.localScale = new Vector3(rhythmSize, rhythmSize, 0);
+            rhythmIndicator.transform.localScale = new Vector3(rhythmSize, 1.0f, rhythmSize);
 
-            rhythmIndicator.color = new Color(1.0f, 1.0f, 1.0f, rhythmCurve.Evaluate(indicatorFactor));
+            rhythmIndicator.sharedMaterial.color = new Color(1.0f, 1.0f, 1.0f, rhythmCurve.Evaluate(indicatorFactor));
 
             var hitSize = hitIndicatorMaxSize - ((hitIndicatorMaxSize - 1) * indicatorFactor);
-            hitIndicator.transform.localScale = new Vector3(hitSize, hitSize, 0);
+            hitIndicator.transform.localScale = new Vector3(hitSize, 1.0f, hitSize);
 
-            var newHitColor = hitIndicator.color;
+            var newHitColor = hitIndicator.sharedMaterial.color;
             newHitColor.a = hitCurve.Evaluate(1.0f - indicatorFactor);
-            hitIndicator.color = newHitColor;
+            hitIndicator.sharedMaterial.color = newHitColor;
         }
     }
 
@@ -84,33 +84,33 @@ public class CursorRhythmIndicator : MonoBehaviour
         switch (state)
         {
             case BeatState.None:
-                hitIndicator.color = noneHitColor;
+                hitIndicator.sharedMaterial.color = noneHitColor;
                 break;
 
             case BeatState.Bad:
-                hitIndicator.color = badHitColor;
+                hitIndicator.sharedMaterial.color = badHitColor;
                 break;
 
             case BeatState.Good:
-                hitIndicator.color = goodHitColor;
+                hitIndicator.sharedMaterial.color = goodHitColor;
                 break;
 
             case BeatState.Great:
-                hitIndicator.color = greatHitColor;
+                hitIndicator.sharedMaterial.color = greatHitColor;
                 break;
 
             case BeatState.Perfect:
-                hitIndicator.color = perfectHitColor;
+                hitIndicator.sharedMaterial.color = perfectHitColor;
                 break;
 
             default:
-                hitIndicator.color = noneHitColor;
+                hitIndicator.sharedMaterial.color = noneHitColor;
                 break;
         }
 
-        if(beatNumber < 3 && state != BeatState.Bad)
+        if (beatNumber <= 3 && state != BeatState.Bad)
         {
-            beatHitSigns[beatNumber].color = hitIndicator.color;
+            beatHitSigns[beatNumber].material.color = hitIndicator.sharedMaterial.color;
         }
         else
         {
@@ -122,7 +122,7 @@ public class CursorRhythmIndicator : MonoBehaviour
     {
         foreach (var bhs in beatHitSigns)
         {
-            bhs.color = noneHitColor;
+            bhs.material.color = noneHitColor;
         }
     }
 
