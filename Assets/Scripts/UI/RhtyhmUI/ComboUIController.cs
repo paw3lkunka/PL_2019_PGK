7aabba50e777857705ba8918f9dee98a22cabb10
Filip Mazurek <f.maz.98@outlook.com>
2020-07-10 16:42:32 +0200

    Added combo UI, added new fail sound

--- Assets/Scripts/UI/RhtyhmUI/ComboUIController.cs
+++ Assets/Scripts/UI/RhtyhmUI/ComboUIController.cs
@@ -2,28 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class ComboUIController : MonoBehaviour
 {
-    private TextMeshProUGUI comboText;
+#pragma warning disable
+    [SerializeField] private Image background;
+    [SerializeField] private Color normalColor;
+    [SerializeField] private Color rageColor;
+    [SerializeField] private float lerpSpeed = 0.05f;
+    [SerializeField] private TextMeshProUGUI comboText;
+    [SerializeField] private TextMeshProUGUI rageText;
+#pragma warning restore
 
-    void Start()
+    private Color currentColor;
+
+    private void Awake()
     {
-        comboText = GetComponent<TextMeshProUGUI>();
+        rageText.enabled = false;
+        comboText.text = "Combo 0";
+        currentColor = normalColor;
     }
 
     private void OnEnable() 
     {
         RhythmMechanics.Instance.OnComboChange += UpdateCombo;
+        RhythmMechanics.Instance.OnRageStart += RageStart;
+        RhythmMechanics.Instance.OnRageStop += RageStop;
     }
 
     private void OnDisable() 
     {
         RhythmMechanics.Instance.OnComboChange -= UpdateCombo;
+        RhythmMechanics.Instance.OnRageStart -= RageStart;
+        RhythmMechanics.Instance.OnRageStop -= RageStop;
     }
 
     private void UpdateCombo(int value)
     {
-        comboText.text = "Combo " + value;
+        if (value <= 99)
+        {
+            comboText.text = "Combo " + value;
+        }
+        else
+        {
+            comboText.text = "Combo ∞";
+        }
+    }
+
+    private void Update()
+    {
+        background.color = Color.Lerp(background.color, currentColor, lerpSpeed);
+    }
+
+    private void RageStart()
+    {
+        currentColor = rageColor; 
+        rageText.enabled = true;
+    }
+
+    private void RageStop()
+    {
+        currentColor = normalColor;
+        rageText.enabled = false;
     }
 }
