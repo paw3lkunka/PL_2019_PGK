e8c403436f472372dd4ebc1eaef9f503f9449548
Ziemniak <potatoosalad@gmail.com>
2020-11-13 15:29:35 +0100

    Adeed drop from enemies - water and faith + custom editor for better setup

---
+++ Assets/Scripts/AgentSystem/Dropable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Damageable))]
+public class Dropable : MonoBehaviour
+{
+    [SerializeField]
+    private float dropChance = 0.0f;
+    [SerializeField]
+    private GameObject spawnedPrefab = null;
+
+    /// <summary>
+    /// How many [prefabs] will drop?
+    /// </summary>
+    [HideInInspector]
+    public Vector2Int dropAmountRange;
+    /// <summary>
+    /// Position range of drop
+    /// </summary>
+    [HideInInspector]
+    public Vector2 dropPositionRange;
+
+    private void Start()
+    {
+        gameObject.GetComponent<Damageable>().Death += Drop;
+    }
+
+    private void Drop()
+    {
+        float random = UnityEngine.Random.Range(0.0f, 100.0f);
+        Vector3 pos = transform.position;
+        if (random < dropChance || random.Equals(dropChance))
+        {
+            for(int i = 0; i < UnityEngine.Random.Range(dropAmountRange.x, dropAmountRange.y); ++i)
+            {
+                Vector3 posMod = new Vector3(UnityEngine.Random.Range(dropPositionRange.x, dropPositionRange.y), 
+                                            0.0f, 
+                                            UnityEngine.Random.Range(dropPositionRange.x, dropPositionRange.y));
+
+                Instantiate(spawnedPrefab, pos + posMod, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/AgentSystem/editor/DropableEditor.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+using UnityEditor.UIElements;
+
+[CustomEditor(typeof(Dropable))]
+public class DropableEditor : Editor
+{
+    private float amountRangeFrom;
+    private float amountRangeTo;
+
+    private float positionRangeFrom;
+    private float positionRangeTo;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        Dropable script = target as Dropable;
+        //set up variables
+        amountRangeFrom = script.dropAmountRange.x;
+        amountRangeTo = script.dropAmountRange.y;
+        positionRangeFrom = script.dropPositionRange.x;
+        positionRangeTo = script.dropPositionRange.y;
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("How Many Prefabs will drop? (Amount Range)", EditorStyles.boldLabel);
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("From: " + script.dropAmountRange.x.ToString());
+        EditorGUILayout.LabelField("To: " + script.dropAmountRange.y.ToString());
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.MinMaxSlider(ref amountRangeFrom, ref amountRangeTo, 0, 15);
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("How Far Prefabs will drop? (Position Range)", EditorStyles.boldLabel);
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("From: " + script.dropPositionRange.x.ToString());
+        EditorGUILayout.LabelField("To: " + script.dropPositionRange.y.ToString());
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.MinMaxSlider(ref positionRangeFrom, ref positionRangeTo, -10, 10);
+
+        if (GUI.changed)
+        {
+            // round to int
+            amountRangeFrom = Mathf.RoundToInt(amountRangeFrom);
+            amountRangeTo = Mathf.RoundToInt(amountRangeTo);
+            script.dropAmountRange.x = Mathf.RoundToInt(amountRangeFrom);
+            script.dropAmountRange.y = Mathf.RoundToInt(amountRangeTo);
+
+
+            // round to 2 decimal places
+            positionRangeFrom = (float)System.Math.Round((double)positionRangeFrom, 1);
+            positionRangeTo = (float)System.Math.Round((double)positionRangeTo, 1);
+            script.dropPositionRange.x = positionRangeFrom;
+            script.dropPositionRange.y = positionRangeTo;
+
+            EditorUtility.SetDirty(script);
+            EditorSceneManager.MarkSceneDirty(script.gameObject.scene);
+        }
+    }
+}
