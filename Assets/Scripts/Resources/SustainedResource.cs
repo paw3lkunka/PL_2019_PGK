bb5d33866ea473a3b82e33ad4e55020cda10e137
Ziemniak <potatoosalad@gmail.com>
2020-12-04 13:36:07 +0100

    Water is no longer added from sources when is full

--- Assets/Scripts/Resources/SustainedResource.cs
+++ Assets/Scripts/Resources/SustainedResource.cs
@@ -53,8 +53,11 @@ public class SustainedResource : MonoBehaviour
             switch (resourceType)
             {
                 case ResourceType.Water:
-                    GameplayManager.Instance.Water += emitted;
-                    textEmitter.Emit(ApplicationManager.Instance.PrefabDatabase.floatingTextResourceGained, emitted.ToString("0.0"), Color.cyan);
+                    if((GameplayManager.Instance.Water.Max - (float)GameplayManager.Instance.Water) > 0.05) 
+                    {
+                        GameplayManager.Instance.Water += emitted;
+                        textEmitter.Emit(ApplicationManager.Instance.PrefabDatabase.floatingTextResourceGained, emitted.ToString("0.0"), Color.cyan);
+                    }
                     break;
                 case ResourceType.Faith:
                     GameplayManager.Instance.Faith += emitted;
