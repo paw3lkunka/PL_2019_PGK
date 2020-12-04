ea5ecad9b1ce318791dabd8f0777b04ed04a6ee6
Filip Mazurek <f.maz.98@outlook.com>
2020-12-04 15:55:10 +0100

    Implemented Alarm in water indicator UI

--- Assets/Scripts/UI/ResourcesUI/ResourcesUIController.cs
+++ Assets/Scripts/UI/ResourcesUI/ResourcesUIController.cs
@@ -9,6 +9,9 @@ public class ResourcesUIController : MonoBehaviour
 #pragma warning disable
     [Header("Water")]
     [SerializeField] private Image waterBar;
+    [SerializeField] private Image waterTop;
+    [SerializeField] private Image warningLight;
+    [SerializeField] private AudioSource waterAlarm;
     [SerializeField] private TextMeshProUGUI waterAmount;
     [SerializeField] private TextMeshProUGUI waterMax;
     [Header("Faith")]
@@ -21,9 +24,28 @@ public class ResourcesUIController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI cultistsAmount;
 #pragma warning restore
 
+    private bool warningEnabled = false;
+
+    private void Start()
+    {
+        warningLight.gameObject.SetActive(false);
+    }
+
     void Update()
     {
         waterBar.fillAmount = GameplayManager.Instance.Water.Normalized;
+        waterTop.enabled = GameplayManager.Instance.Water.Normalized > 0;
+        if (GameplayManager.Instance.Water.Normalized < GameplayManager.Instance.lowWaterLevel && !warningEnabled)
+        {
+            warningLight.gameObject.SetActive(true);
+            warningEnabled = true;
+            waterAlarm.Play();
+        }
+        else if (GameplayManager.Instance.Water.Normalized >= GameplayManager.Instance.lowWaterLevel && warningEnabled)
+        {
+            warningLight.gameObject.SetActive(false);
+            warningEnabled = false;
+        }
         waterAmount.text = System.Math.Round(GameplayManager.Instance.Water, 1).ToString("000.0").Remove(3, 1).Insert(3, "<color=black>").Insert(0, "<mspace=8.0>");
         waterMax.text = System.Math.Round(GameplayManager.Instance.Water.Max, 1).ToString("000.0").Remove(3, 1).Insert(3, "<color=black>").Insert(0, "<mspace=8.0>");
 
