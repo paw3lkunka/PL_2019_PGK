73ea0f6312e28a89d8c79a1c403524f1230adc17
Filip Mazurek <f.maz.98@outlook.com>
2020-12-18 15:39:03 +0100

    Updated main menu, game over, game won and intro scenes

---
+++ Assets/Scripts/Utility/LoadSceneOnAnyKey.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class LoadSceneOnAnyKey : MonoBehaviour
+{
+    public int sceneIndex = 0;
+
+    public void StartScene()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Update()
+    {
+        if (Keyboard.current.anyKey.wasPressedThisFrame)
+        {
+            StartScene();
+        }
+
+        if (Gamepad.current != null && Gamepad.current.allControls.Any(x => x is ButtonControl button && x.IsPressed() && !x.synthetic))
+        {
+            StartScene();
+        }
+    }
+}
