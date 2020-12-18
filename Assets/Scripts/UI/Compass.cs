45ddfdbdbea5536f438c4811595c7650ceb01a23
Filip Mazurek <f.maz.98@outlook.com>
2020-12-18 17:16:55 +0100

    World map compass fix

--- Assets/Scripts/UI/Compass.cs
+++ Assets/Scripts/UI/Compass.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,24 +7,15 @@ public class Compass : MonoBehaviour
 {
     public Transform shield;
 
-    private Vector3 oldDirection;
+    private Transform cameraPivot;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        oldDirection = Vector3.forward;
-        shield.rotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+        cameraPivot = Camera.main.GetComponentInParent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 cameraDirection = Camera.main.transform.forward;
-
-        float angle = Vector3.SignedAngle(oldDirection, cameraDirection, Vector3.up);
-        
-        shield.Rotate(Vector3.forward, angle);
-
-        oldDirection = cameraDirection;
+        shield.rotation = Quaternion.Euler(0.0f, 0.0f, cameraPivot.rotation.eulerAngles.y);
     }
 }
