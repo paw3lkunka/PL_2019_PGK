14bf41013c89cc7ad1e1fc05b9b215f5366a374c
Aandrzejmi <andrzej.mi@op.pl>
2021-01-08 15:50:29 +0100

    Linear speed and attack depending on faith

--- Assets/Scripts/AgentSystem/CultLeader.cs
+++ Assets/Scripts/AgentSystem/CultLeader.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class CultLeader : MonoBehaviour
 {
+    private Moveable moveable;
+
+    public Vector2 speedMinMax;
 
     #region MonoBehaviour
     private void Awake()
     {
         LocationManager.Instance.cultLeader = this;
+        moveable = GetComponent<Moveable>();
+    }
+
+    private void Update()
+    {
+        float faith = GameplayManager.Instance.Faith.Normalized;
+
+        moveable.SpeedBase = Mathf.Lerp(speedMinMax.x, speedMinMax.y, faith);
     }
 
     #endregion
--- Assets/Scripts/AgentSystem/interfaces/IAttack.cs
+++ Assets/Scripts/AgentSystem/interfaces/IAttack.cs
@@ -2,7 +2,7 @@
 
 public interface IAttack : IMonoBehaviour
 {
-
+    float DamageBaseMultiplier { get; set; }
     void Attack(Vector3 target);
     void HoldFire();
 }
