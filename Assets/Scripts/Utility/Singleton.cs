0a496b5dfb783e8131f8755251c81e0695b7d514
Filip Mazurek <f.maz.98@outlook.com>
2021-02-26 16:41:34 +0100

    Added world map stars, fixed strange singleton bug

--- Assets/Scripts/Utility/Singleton.cs
+++ Assets/Scripts/Utility/Singleton.cs
@@ -86,7 +86,7 @@ public abstract class Singleton<T, K> : Singleton where T : MonoBehaviour where 
 
 public abstract class Singleton : MonoBehaviour
 {
-    public static bool Quitting { get; private set; }
+    public static bool Quitting { get; private set; } = false;
 
     private void OnApplicationQuit() 
     {
